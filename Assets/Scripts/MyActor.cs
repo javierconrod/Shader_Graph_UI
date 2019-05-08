0d72ec6dcceea75563b52c67f84ded397966a2a2
Javier Contreras <javierin_kl@hotmail.com>
2019-05-08 00:06:47 -0700

    Initial commit

---
+++ Assets/Scripts/MyActor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyActor : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    private float colorPower = 1f;
+    [SerializeField]
+    Slider sldColorPower;
+    [SerializeField]
+    Slider sldNormalAmount;
+    [SerializeField]
+    Slider sldRimPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.SetFloat("_ColorPower", sldColorPower.value);
+        render.material.SetFloat("_NormalAmount", sldNormalAmount.value);
+        render.material.SetFloat("_RimPower", sldRimPower.value);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Shader_Graph_UI
